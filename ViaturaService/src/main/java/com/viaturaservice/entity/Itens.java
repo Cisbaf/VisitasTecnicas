b196648f44a994be73ab8bc59d2160b4a2a83296
GugsFranca <guto602@gmail.com>
2025-07-31 13:51:23 -0300

    feat(services): add VisitaService and update BaseService references

--- ViaturaService/src/main/java/com/viaturaservice/entity/Itens.java
+++ ViaturaService/src/main/java/com/viaturaservice/entity/Itens.java
@@ -1,10 +1,14 @@
 package com.viaturaservice.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Itens implements Serializable {
     private String nome;
     private int conformidade;

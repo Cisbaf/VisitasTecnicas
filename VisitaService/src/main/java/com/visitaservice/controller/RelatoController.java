e2b487de17cc69d5d00707bbe945626a2e419920
GugsFranca <guto602@gmail.com>
2025-08-04 14:42:27 -0300

    feat(relato): implement RelatoController and RelatoServiceImp for CRUD operations

---
+++ VisitaService/src/main/java/com/visitaservice/controller/RelatoController.java
@@ -0,0 +1,62 @@
+package com.visitaservice.controller;
+
+import com.visitaservice.entity.dto.relato.RelatoRequest;
+import com.visitaservice.entity.dto.relato.RelatoResponse;
+import com.visitaservice.service.capsule.RelatoService;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/relatos")
+@RequiredArgsConstructor
+public class RelatoController {
+    private final RelatoService service;
+
+    @GetMapping
+    @Operation(summary = "Get All Relatos", description = "Retrieve a list of all relatos.")
+    public ResponseEntity<List<RelatoResponse>> getAll() {
+        List<RelatoResponse> relatos = service.getAll();
+        if (relatos.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(relatos);
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Get Relato by ID", description = "Retrieve a relato by its unique identifier.")
+    public ResponseEntity<RelatoResponse> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(service.getById(id));
+    }
+
+    @PostMapping
+    @Operation(summary = "Create a new Relato", description = "Create a new relato with the provided details.")
+    public ResponseEntity<RelatoResponse> create(@RequestBody @Valid RelatoRequest request) {
+        if (request == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        RelatoResponse response = service.createRelato(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{id}")
+    @Operation(summary = "Update an existing Relato", description = "Update the details of an existing relato by its ID.")
+    public ResponseEntity<RelatoResponse> update(@PathVariable Long id, @RequestBody @Valid RelatoRequest request) {
+        if (request == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        RelatoResponse response = service.updateRelato(id, request);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a Relato", description = "Delete a relato by its ID.")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ VisitaService/src/main/java/com/visitaservice/service/relato/RelatoServiceImp.java
@@ -0,0 +1,56 @@
+package com.visitaservice.service.relato;
+
+import com.visitaservice.entity.RelatoEntity;
+import com.visitaservice.entity.dto.relato.RelatoRequest;
+import com.visitaservice.entity.dto.relato.RelatoResponse;
+import com.visitaservice.repository.RelatoRepository;
+import com.visitaservice.service.capsule.RelatoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static com.visitaservice.service.relato.RelatoMapper.toResponse;
+
+@Service
+@RequiredArgsConstructor
+public class RelatoServiceImp implements RelatoService {
+    private final RelatoRepository repository;
+    private final RelatoMapper mapper;
+    private final String RELATO_NOT_FOUND = "Relato não encontrado com o id: ";
+
+    public RelatoResponse createRelato(RelatoRequest request) {
+        RelatoEntity entity = mapper.toEntity(request);
+        RelatoEntity savedEntity = repository.save(entity);
+        return toResponse(savedEntity);
+    }
+
+    public RelatoResponse getById(Long id) {
+        return repository.findById(id)
+                .map(RelatoMapper::toResponse)
+                .orElseThrow(() -> new IllegalArgumentException(RELATO_NOT_FOUND + id));
+    }
+    public List<RelatoResponse> getAll() {
+        return repository.findAll().stream()
+                .map(RelatoMapper::toResponse)
+                .toList();
+    }
+
+    public RelatoResponse updateRelato( Long id, RelatoRequest request) {
+        if (!repository.existsById(id)) {
+            throw new IllegalArgumentException(RELATO_NOT_FOUND + id);
+        }
+        RelatoEntity entity = repository.findById(id).orElseThrow();
+        BeanUtils.copyProperties(request, entity, "id");
+        RelatoEntity updatedEntity = repository.save(entity);
+        return toResponse(updatedEntity);
+    }
+
+    public void delete(Long id) {
+        if (!repository.existsById(id)) {
+            throw new IllegalArgumentException(RELATO_NOT_FOUND + id);
+        }
+        repository.deleteById(id);
+    }
+}

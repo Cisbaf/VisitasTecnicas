1bc4e97d722f5a01014ad486c16806953fe85f88
GugsFranca <guto602@gmail.com>
2025-08-06 11:28:37 -0300

    refactor(CheckList): Modificando a forma como conformidade é salvo utilizando logica

--- CheckListItemService/src/main/java/com/checklistitemservice/service/CheckListMapper.java
+++ CheckListItemService/src/main/java/com/checklistitemservice/service/CheckListMapper.java
@@ -3,6 +3,7 @@ package com.checklistitemservice.service;
 import com.checklistitemservice.entity.CheckListEntity;
 import com.checklistitemservice.entity.dto.CheckListRequest;
 import com.checklistitemservice.entity.dto.CheckListResponse;
+import com.checklistitemservice.entity.enums.TipoConformidade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -26,9 +27,21 @@ class CheckListMapper {
         if (request == null) {
             return null;
         }
+        var descricao = request.descricao();
+        for( var desc : descricao) {
+            if (desc.getConformidadePercent() <= 44) {
+                desc.setTipoConformidade(TipoConformidade.NAO_CONFORME);
+            } else if (desc.getConformidadePercent() >= 70) {
+                desc.setTipoConformidade(TipoConformidade.CONFORME);
+            } else {
+                desc.setTipoConformidade(TipoConformidade.PARCIAL);
+
+            }
+        }
+
         return CheckListEntity.builder()
                 .categoria(request.categoria())
-                .descricao(request.descricao())
+                .descricao(descricao)
                 .visitaId(request.visitaId())
                 .build();
     }

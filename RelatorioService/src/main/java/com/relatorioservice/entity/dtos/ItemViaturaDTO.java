9ef78d4787cb39bea2849fc0127ed07d0b5d2b23
GugsFranca <guto602@gmail.com>
2025-08-06 11:32:25 -0300

    refactor(relatorio): Modificando a logica de recebimento de relatorios removendo a necessidade de salvamento em banco de dados

--- RelatorioService/src/main/java/com/relatorioservice/entity/dtos/ItemViaturaDTO.java
+++ RelatorioService/src/main/java/com/relatorioservice/entity/dtos/ItemViaturaDTO.java
@@ -1,11 +1,9 @@
 package com.relatorioservice.entity.dtos;
 
-import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Embeddable
 @Data
 @NoArgsConstructor
 @AllArgsConstructor

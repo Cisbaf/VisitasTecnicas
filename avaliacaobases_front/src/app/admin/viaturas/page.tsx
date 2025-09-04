9857b310e8c7ee0c8e126a0cfc6594ab2d9a544d
GugsFranca <guto602@gmail.com>
2025-09-04 10:14:25 -0300

    refactor: improving organization

--- avaliacaobases_front/src/app/admin/viaturas/page.tsx
+++ avaliacaobases_front/src/app/admin/viaturas/page.tsx
@@ -1,7 +1,7 @@
 // app/base/[baseId]/page.tsx
-import {cookies} from 'next/headers';
-import {decodeJwtPayload} from '../../../lib/decodeJwt';
-import {redirect} from 'next/navigation';
+import { cookies } from 'next/headers';
+import { decodeJwtPayload } from '../../../lib/decodeJwt';
+import { redirect } from 'next/navigation';
 import AdminViatura from '@/components/admin/AdminViatura';
 
 
@@ -20,5 +20,5 @@ export default async function BasePage() {
         redirect('/login');
     }
 
-    return <AdminViatura/>
+    return <AdminViatura />
 }

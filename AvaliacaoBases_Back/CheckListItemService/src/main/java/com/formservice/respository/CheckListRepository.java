668de489f60eeac55e92cfafcaf1bd4da9ee4f9b
GugsFranca <guto602@gmail.com>
2025-09-30 10:18:20 -0300

    remove: Checklist module

--- AvaliacaoBases_Back/CheckListItemService/src/main/java/com/formservice/respository/CheckListRepository.java
+++
@@ -1,9 +0,0 @@
-package com.formservice.respository;
-
-import com.formservice.entity.CheckListEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CheckListRepository extends JpaRepository<CheckListEntity, Long> {
-    boolean existsByCategoria(String categoria);
-
-}

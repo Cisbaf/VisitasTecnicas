cd07473add6ff3b6a43c5f8ad03270cf5d1a83a8
GugsFranca <guto602@gmail.com>
2025-10-14 12:00:00 -0300

    feat: remove unused Itens references from ViaturaControllerTeste

--- AvaliacaoBases_Back/ViaturaService/src/test/java/com/viaturaservice/ViaturaControllerTeste.java
+++ AvaliacaoBases_Back/ViaturaService/src/test/java/com/viaturaservice/ViaturaControllerTeste.java
@@ -39,14 +39,12 @@ class ViaturaControllerTest {
                 .tipoViatura("Patrulha")
                 .statusOperacional("Ativo")
                 .idBase(1L)
-                .itens(List.of(new Itens()))
                 .build(), ViaturaResponse.builder()
                 .id(2L)
                 .placa("ABC5678")
                 .tipoViatura("Patrulha")
                 .statusOperacional("Ativo")
                 .idBase(1L)
-                .itens(List.of(new Itens()))
                 .build());
         when(viaturaService.getAllViaturas()).thenReturn(viaturas);
 
@@ -64,7 +62,6 @@ class ViaturaControllerTest {
                 .tipoViatura("Patrulha")
                 .statusOperacional("Ativo")
                 .idBase(1L)
-                .itens(List.of(new Itens()))
                 .build();
         when(viaturaService.getViaturaById(1L)).thenReturn(viatura);
 
@@ -99,7 +96,6 @@ class ViaturaControllerTest {
                 .tipoViatura("Patrulha")
                 .statusOperacional("Ativo")
                 .idBase(1L)
-                .itens(List.of(new Itens()))
                 .build();
         when(exists.existsById(viatura.idBase())).thenReturn(false);
 
@@ -135,7 +131,6 @@ class ViaturaControllerTest {
                 .tipoViatura("Patrulha")
                 .statusOperacional("Ativo")
                 .idBase(1L)
-                .itens(List.of(new Itens()))
                 .build());
 
         assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
@@ -157,7 +152,6 @@ class ViaturaControllerTest {
                 .tipoViatura("Patrulha")
                 .statusOperacional("Ativo")
                 .idBase(1L)
-                .itens(List.of(new Itens()))
                 .build();
         ViaturaResponse updatedViatura = ViaturaResponse.builder()
                 .id(1L)
@@ -165,7 +159,6 @@ class ViaturaControllerTest {
                 .tipoViatura("Patrulha")
                 .statusOperacional("Ativo")
                 .idBase(1L)
-                .itens(List.of(new Itens()))
                 .build();
         when(viaturaService.updateViatura(1L, viatura)).thenReturn(updatedViatura);
 

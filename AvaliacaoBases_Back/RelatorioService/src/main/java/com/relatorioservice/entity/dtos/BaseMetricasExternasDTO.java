47302b3def107c96ef7ffd43f8156753c0d53cb5
GugsFranca <guto602@gmail.com>
2025-10-15 14:07:10 -0300

    feat: add BaseMetricasExternasDTO and VtrMediaDto; implement InspecaoServiceClient for external service integration

---
+++ AvaliacaoBases_Back/RelatorioService/src/main/java/com/relatorioservice/entity/dtos/BaseMetricasExternasDTO.java
@@ -0,0 +1,16 @@
+package com.relatorioservice.entity.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BaseMetricasExternasDTO {
+    private String baseNome;
+    private Long idBase;
+    private Double porcentagemVtrAtiva;
+    private String tempoMedioProntidao;
+    private String tempoMedioAtendimento;
+}
--- AvaliacaoBases_Back/RelatorioService/src/main/java/com/relatorioservice/entity/dtos/VtrMediaDto.java
+++ AvaliacaoBases_Back/RelatorioService/src/main/java/com/relatorioservice/entity/dtos/VtrMediaDto.java
@@ -1,13 +1,16 @@
 package com.relatorioservice.entity.dtos;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ItemViaturaDTO {
-    private String nome;
-    private int conformidade;
+public class VtrMediaDto{
+    private String cidade;
+    private double ativa;
+
 }

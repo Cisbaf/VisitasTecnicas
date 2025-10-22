152328109c4eb8958d79f7c7666087777f20eade
GugsFranca <guto602@gmail.com>
2025-10-22 13:44:41 -0300

    feat: add ViaturaPage component; integrate InspecaoPage for vehicle inspection display

---
+++ avaliacaobases_front/src/app/base/[baseId]/inspecao/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import InspecaoPage from "@/components/base/InspecaoPage";
+
+export default async function ViaturaPage() {
+
+    return (
+        <InspecaoPage />
+    );
+}
